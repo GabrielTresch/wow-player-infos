79fabf2299d4ccff575654cf248e4501ba490b6b
GabrielTresch <gabriel_tresch@outlook.com>
2019-10-12 12:59:11 +0200

    achievement category

--- src/components/WowHome.js
+++ src/components/WowHome.js
@@ -17,12 +17,14 @@ const GetUser = () => {
     user: [],
     race: [],
     realm: [],
+    specActive: [],
     spec: [],
     media: [],
     titles: [],
     mounts: [],
     reputation: [],
     pvp: [],
+    achiev: [],
   });
   useEffect(() => {
     async function fetchData() {
@@ -43,14 +45,58 @@ const GetUser = () => {
       const realm = await axios(profil.data.realm.key.href, header);
       const media = await axios(profil.data.media.href, header);
       // Specialization
-      const spec = await axios(profil.data.active_spec.key.href, header);
+      const specActive = await axios(profil.data.active_spec.key.href, header);
+      const spec = await axios(profil.data.specializations.href, header);
       // Titles
       const titles = await axios(profil.data.titles.href, header);
       // Réputations
       const reputation = await axios(profil.data.reputations.href, header);
       // PVP
       const pvp = await axios(profil.data.pvp_summary.href, header);
-      console.log(pvp);
+      // Achievement
+      const achievement = await axios(profil.data.achievements.href, header);
+      const categoryArray = [];
+      const parentCategoryArray = [];
+      achievement.data.category_progress.forEach(async (e, i) => {
+        setTimeout(async () => {
+          // console.log(e);
+          const category = await axios(e.category.key.href, header);
+          // console.log(category.data);
+          // eslint-disable-next-line max-len
+          // console.log(category.data.parent_category ? category.data.parent_category.name.fr_FR : false);
+          categoryArray.push({
+            id: i,
+            categoryParent: category.data.parent_category
+              ? category.data.parent_category.name.fr_FR : '',
+            category: category.data.name.fr_FR,
+          });
+          if (category.data.parent_category === undefined) {
+            parentCategoryArray.push(category.data.name.fr_FR);
+          }
+          // console.log(parentCategoryArray);
+          parentCategoryArray.forEach((el) => {
+            console.log(el);
+            // eslint-disable-next-line array-callback-return
+            const test = categoryArray.filter((element) => element.categoryParent === el);
+            console.log(test);
+          });
+          // console.log(categoryArray);
+          // setWow({
+          //   user: profil.data,
+          //   race: race.data,
+          //   realm: realm.data,
+          //   specActive: specActive.data,
+          //   spec: spec.data,
+          //   media: media.data,
+          //   titles: titles.data,
+          //   reputation: reputation.data,
+          //   pvp: pvp.data,
+          //   achiev: [...categoryArray],
+          // });
+        }, i * 100);
+      });
+      // console.log(categoryArray);
+      // eslint-disable-next-line max-len
       // Collection
       // const collection = await axios(profil.data.collections.href, header);
       // const getMounts = await axios(collection.data.mounts.href, header);
@@ -58,18 +104,19 @@ const GetUser = () => {
       // getMounts.data.mounts.forEach(async (e, i) => {
       //   setTimeout(async () => {
       //     const mount = await axios(e.mount.key.href, header);
-      //     const mountImages = await axios(mount.data.creature_displays[0].key.href, header);
+      //     const mountImg = await axios(mount.data.creature_displays[0].key.href, header);
       //     mountArray.push({
       //       id: i,
       //       name: mount.data.name.fr_FR,
       //       description: mount.data.description.fr_FR,
       //       source: mount.data.source ? mount.data.source.name.fr_FR : '',
-      //       img: mountImages.data.assets[2].value,
+      //       img: mountImg.data.assets[2].value,
       //     });
       //     setWow({
       //       user: profil.data,
       //       race: race.data,
       //       realm: realm.data,
+      //       specActive: specActive.data,
       //       spec: spec.data,
       //       media: media.data,
       //       mounts: [...mountArray],
@@ -83,6 +130,7 @@ const GetUser = () => {
         user: profil.data,
         race: race.data,
         realm: realm.data,
+        specActive: specActive.data,
         spec: spec.data,
         media: media.data,
         titles: titles.data,
@@ -104,8 +152,8 @@ const GetUser = () => {
               <img src={wow.media.bust_url} alt="avatar wow" />
               <div>{`Name: ${wow.user.name}`}</div>
               <div>{`Race: ${wow.race.name.fr_FR}`}</div>
-              <div>{`Class: ${wow.spec.playable_class.name.fr_FR}`}</div>
-              <div>{`Specialization: ${wow.spec.name.fr_FR}`}</div>
+              <div>{`Class: ${wow.specActive.playable_class.name.fr_FR}`}</div>
+              <div>{`Specialization: ${wow.specActive.name.fr_FR}`}</div>
               <div>{`Faction: ${wow.race.faction.name.fr_FR}`}</div>
               <div>{`Level: ${wow.user.level}`}</div>
               <div>{`ILevel: ${wow.user.average_item_level}`}</div>
@@ -113,8 +161,8 @@ const GetUser = () => {
             </div>
             <div>
               <h2>Specialization</h2>
-              <div>{`Class: ${wow.spec.playable_class.name.fr_FR}`}</div>
-              <div>{`Specialization: ${wow.spec.name.fr_FR}`}</div>
+              <div>{`Class: ${wow.specActive.playable_class.name.fr_FR}`}</div>
+              <div>{`Specialization: ${wow.specActive.name.fr_FR}`}</div>
             </div>
             <div>
               <h2>Mount</h2>
