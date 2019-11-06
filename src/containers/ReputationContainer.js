2bdd7a6659550b2be9801e5d1b12603bff5ab4c5
GabrielTresch <gabriel_tresch@outlook.com>
2019-11-06 18:50:11 +0100

    reputations

--- src/containers/ReputationContainer.js
+++ src/containers/ReputationContainer.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import request from '../api/Request';
+import AxiosHeader from '../api/AxiosHeader';
+import AxiosAuth from '../api/AxiosAuth';
 
-const ReputationContainer = () => (
-  <h1>Réputations</h1>
-);
+const ReputationContainer = () => {
+  // const [reputation, setReputation] = useState([]);
+
+  const pseudo = useSelector((state) => state.profil.pseudo);
+  const realmSlug = useSelector((state) => state.profil.realmslug);
+  const region = useSelector((state) => state.profil.region);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const Auth = AxiosAuth();
+      const getToken = await request('https://eu.battle.net/oauth/token', Auth);
+      const header = AxiosHeader(getToken.data.access_token);
+      const getProfil = await request(`https://${region}.api.blizzard.com/profile/wow/character/${realmSlug}/${pseudo}?namespace=profile-${region}&locale=fr_EU`, header);
+
+      const reputations = await request(getProfil.data.reputations.href, header);
+      console.log(reputations);
+    };
+    if (pseudo !== undefined) {
+      fetchData();
+    }
+  }, [pseudo, realmSlug, region]);
+  // console.log(reputation);
+  return (
+    <>
+      <h1>Réputation</h1>
+    </>
+  );
+};
 
 export default ReputationContainer;
