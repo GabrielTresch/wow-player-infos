c60016b1e946e064d56e1c7811b25bc62c2997c2
GabrielTresch <gabriel_tresch@outlook.com>
2019-11-07 19:37:59 +0100

    token remove

--- src/api/Mounts.js
+++ src/api/Mounts.js
@@ -2,7 +2,7 @@ import request from '../utils/Request';
 import AxiosHeader from '../utils/AxiosHeader';
 
 const Mounts = async (href) => {
-  const header = AxiosHeader('EU7Wx4G8YohqmEydkYQdUC5gt8rXuroV01');
+  const header = AxiosHeader('');
   const mount = await request(href, header);
   const mountImg = await request(mount.data.creature_displays[0].key.href, header);
 
