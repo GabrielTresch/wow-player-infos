edbc8e5e7f7a8b524c9aaefc82bff88a53010256
GabrielTresch <gabriel_tresch@outlook.com>
2019-11-14 19:33:35 +0100

    navigations modifs

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
     <NavLink to="/raid-progression">Raid progression </NavLink>
     <NavLink to="/collection/montures">Collection </NavLink>
     <NavLink to="/reputations">Réputation </NavLink>
-    <NavLink to="/achevements">Achèvements </NavLink>
+    <NavLink to="/achevements">Hauts Faits </NavLink>
     <a href="https://discordapp.com/api/oauth2/authorize?client_id=640593176473763841&permissions=317504&scope=bot" target="blank">bot discord</a>
   </nav>
 );
