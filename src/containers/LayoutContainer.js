b5d35433efc28d303495b7af4228f1b9abe7bb6c
GabrielTresch <gabriel_tresch@outlook.com>
2019-11-15 14:51:22 +0100

    main layout

--- src/containers/LayoutContainer.js
+++ src/containers/LayoutContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from '../components/Navigation';
 import Search from '../components/Search';
+import './LayoutContainer.scss';
 
 const LayoutContainer = ({ children }) => (
   <div className="App">
