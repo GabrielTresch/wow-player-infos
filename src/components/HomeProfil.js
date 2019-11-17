1f9fc9dae03cfdfb15f470452f69ce1dd16b9715
GabrielTresch <gabriel_tresch@outlook.com>
2019-11-17 15:13:02 +0100

    profil final informations

--- src/components/HomeProfil.js
+++ src/components/HomeProfil.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import iconHorde from '../assets/img/horde.png';
+import iconAlliance from '../assets/img/alliance.png';
 
 const HomeProfil = ({
   profil, media, race, realm, activSpe, activTitle,
 }) => (
   <>
-    {console.log(activTitle, '---', activSpe)}
     {profil.level !== undefined
       && profil.average_item_level !== undefined
       && profil.name !== undefined
       && race.name !== undefined
       && realm.name !== undefined
       && media.bust_url !== undefined
+      && media.render_url !== undefined
       && race.faction.name !== undefined
       && activTitle.name.fr_FR !== undefined
       ? (
@@ -19,14 +21,13 @@ const HomeProfil = ({
           <h2>Profil</h2>
           <img src={media.bust_url} alt="avatar wow" />
           <img src={media.render_url} alt="personnage wow" />
-          <div>{`Name: ${profil.name}`}</div>
-          <div>{`Title: ${activTitle.name.fr_FR}`}</div>
+          <div>{`${activTitle.name.fr_FR} ${profil.name}`}</div>
+          <div>{`Niveau ${profil.level}`}</div>
           <div>{`${activSpe.playable_class.name.fr_FR} ${activSpe.name.fr_FR}`}</div>
-          <div>{`Race: ${race.name.fr_FR}`}</div>
-          <div>{`Faction: ${race.faction.name.fr_FR}`}</div>
-          <div>{`Level: ${profil.level}`}</div>
-          <div>{`ILevel: ${profil.average_item_level}`}</div>
-          <div>{`Realm: ${realm.name.fr_FR}`}</div>
+          <div>
+            {race.faction.name.fr_FR === 'Horde' ? <img src={iconHorde} alt="hord icon" /> : <img src={iconAlliance} alt="alliance icon" />}
+            <span>{realm.name.fr_FR}</span>
+          </div>
         </div>
       )
       : <p>Loading...</p>}
