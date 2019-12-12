fe4549878823114a98e6277c4e19e1ddd866717b
GabrielTresch <gabriel_tresch@outlook.com>
2019-12-12 16:15:32 +0100

    leaderboard style bug fix

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -3,24 +3,27 @@ import PropTypes from 'prop-types';
 import './Leaderboard.scss';
 
 const Leaderboard = ({ leaderboard }) => (
-  <table className="leaderboard">
-    <thead>
-      <tr>
-        <th>rank</th>
-        <th>guild</th>
-        <th>realm</th>
-      </tr>
-    </thead>
-    <tbody>
-      {leaderboard.map((value, i) => (
-        <tr key={value.date}>
-          <td>{i + 1}</td>
-          <td style={{ color: `var(--${value.faction})` }}>{value.guild}</td>
-          <td>{`${value.region.toUpperCase()} - ${value.realm}`}</td>
+  <div className="row-wrapper">
+    <table className="leaderboard">
+      <thead>
+        <tr>
+          <th>rank</th>
+          <th>guild</th>
+          <th>realm</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+
+        {leaderboard.map((value, i) => (
+          <tr key={value.date}>
+            <td>{i + 1}</td>
+            <td style={{ color: `var(--${value.faction})` }}>{value.guild}</td>
+            <td>{`${value.region.toUpperCase()} - ${value.realm}`}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 Leaderboard.propTypes = {
